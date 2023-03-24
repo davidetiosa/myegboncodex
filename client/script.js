d51cfc844cfaa85c9b076ae9f65bb2102518cfa6
David Etiosa Ojo <davidetiosaojo@gmail.com>
2023-03-24 18:43:19 +0100

    key changes

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://myegbon-server.onrender.com', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
